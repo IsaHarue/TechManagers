7b600d93d5e611b6db849a70d041e1da6c16c8e5
Medeiros <gustavo.silva125@portalsesisp.org.br>
2024-08-27 10:38:35 -0300

    criação das rotas do usuário e item

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from urllib import request
 
-from flask import Flask, json
+from flask import Flask
 
 from models import Funcionario, db_session
 
@@ -12,24 +12,21 @@ def hello_world():  # put application's code here socorro meu deus alguem me aju
     return 'Hello World!'
 
 
-@app.route('/funcionario/add', methods=['POST'])
-def add_funcionario():
-    """Pagina para adicionar Funcionarios
-    Para adiciona-la o adin deve listar o nome da pessoa junto com o email dela e o CPF e sua senha pessoal
-    """
-    pessoa = Pessoa(nome=request.form['nome'],
-                    cpf=request.form['cpf'],
-                    email=request.form['email'],
-                    senha=request.form['senha'])
-
-    db.session.add(pessoa)
-    pessoa.save()
+@app.route('/item/add', methods=['POST'])
+def add_item():
+    item = Item(nome=request.form['nome'],
+                tipo=request.form['tipo'],
+                quantidade=request.form['quantidade'])
+    db_session.add(item)
+    item.save()
     final = {
-        'status': 'ok',
-        'nome': pessoa.nome,
-        'cpf': pessoa.cpf,
-        'email': pessoa.email,
-        'senha': pessoa.senha
+        'status': 'success',
+        'nome': item.nome,
+        'tipo': item.tipo,
+        'quantidade': item.quantidade}
+    return app.response_class(response=json.dumps(final),
+                              status=201,
+                              mimetype='application/json')
     }
     return app.response_class()
 
@@ -52,6 +49,53 @@ def update_funcionario(id):
         return app.response_class(
 
         )
+
+@app.route('/item/update/<int:id>', methods=['PUT'])
+def update_item(id):
+        item = select(Item).where(Item.id == id)
+        item = db_session.execute(item).scalar()
+        item.nome = request.form['nome']
+        item.tipo = request.form['tipo']
+        item.quantidade = request.form['quantidade']
+        db_session.commit()
+        final = {
+            'status': 'success',
+            'nome': item.nome,
+            'tipo': item.tipo,
+            'quantidade': item.quantidade}
+
+        return Response(response=json.dumps(final),
+
+                        status=201,
+                        mimetype='application/json')
+
+@app.route('/item/delete/<int:id>', methods=['DELETE'])
+def delete_item(id):
+    item = select(Item).where(Item.id == id)
+    item = db_session.execute(item).scalar()
+    db.session.add(pessoa)
+    pessoa.save()
+    final = {
+        'status': 'removido',
+        'nome': item.nome,
+        'tipo': item.tipo,
+        'quantidade': item.quantidade}
+    db_session.delete(item)
+    db_session.commit()
+    return Response(response=json.dumps(final),
+                    status=201,
+                    mimetype='application/json')
+
+@app.route('/item/detalhes', methods=['GET'])
+def detalhes_item():
+    item = Item.query.all()
+    result = []
+    for consulta in item:
+        result.append(consulta.serialize_item())
+    final = json.dumps(result)
+    return Response(response=final,
+                    status=201,
+                    content_type='application/json')
 
 @app.route (
 
